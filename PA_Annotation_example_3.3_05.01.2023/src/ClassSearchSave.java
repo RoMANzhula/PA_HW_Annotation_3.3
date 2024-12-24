03a0f1ee61b97779600dcce7bb9b8e1ba8704cff
Roman Manzhula <roman1985al@ukr.net>
2024-12-24 21:55:28 +0200

    Update ClassSearchSave.java
    In Ukrainian

--- PA_Annotation_example_3.3_05.01.2023/src/ClassSearchSave.java
+++ PA_Annotation_example_3.3_05.01.2023/src/ClassSearchSave.java
@@ -5,35 +5,36 @@ import java.lang.reflect.Method;
 
 public class ClassSearchSave {
     private static final File file = new File("C:\\Users\\Admin\\Desktop\\Новый текстовый документ.txt");
-//    public static boolean searchAnnotationsSaveForClasses(Class<?>... listClasses) throws InvocationTargetException, IllegalAccessException { //на вход
-//        // метода передаем список классов, в которых будем искать аннотацию
-//        try {
-//            for (Class<?> clazz : listClasses) { //проходимся по каждому классу
-//                Method[] methods = clazz.getDeclaredMethods(); //получаем список методов каждого класса
-//                for (Method method : methods) { //проходим по каждому методу
-//                    if (method.isAnnotationPresent(Save.class)) { //если метод isAnnotationPresent аннотирован аннотацией Save
-//                        //то мы этот метод invoke() - вызываем с помощью Рефлексии
-//                        Boolean b = (Boolean) method.invoke(null); //null передается, посколько методы принидутельно статические
-//                        if (!b) { //если метод вернул false - то останавливаемся
-//                            return false; //если вернул true - то Save прошел(присутствует) и идем дальше
-//                        }
-//                    }
-//                }
-//            }
-//            return true; //все Save прошли успешно
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return false;
-//        }
-//    }
+
+    //    public static boolean searchAnnotationsSaveForClasses(Class<?>... listClasses) throws InvocationTargetException, IllegalAccessException { 
+    //на вхід передаємо список класів, в яких будемо шукати анотацію
+    //        try {
+    //            for (Class<?> clazz : listClasses) { //проходимо по кожному класу
+    //                Method[] methods = clazz.getDeclaredMethods(); //отримуємо список методів кожного класу
+    //                for (Method method : methods) { //проходимо по кожному методу
+    //                    if (method.isAnnotationPresent(Save.class)) { //якщо метод позначено анотацією Save
+    //                        //то ми цей метод invoke() - викликаємо через рефлексію
+    //                        Boolean b = (Boolean) method.invoke(null); //null передається, оскільки методи статичні
+    //                        if (!b) { //якщо метод повернув false - зупиняємось
+    //                            return false; //якщо повернув true - то Save пройшов (присутня) і йдемо далі
+    //                        }
+    //                    }
+    //                }
+    //            }
+    //            return true; //всі Save пройшли успішно
+    //        } catch (Exception e) {
+    //            e.printStackTrace();
+    //            return false;
+    //        }
+    //}
 
     public static void searchAnnotationsSaveForFields(Class clazz) throws Exception, IOException {
         try {
-            Field[] fields = clazz.getDeclaredFields(); //получауем список полей класса
-            for (Field field : fields) { //проходимся по каждому полю класса
-                if (field.isAnnotationPresent(Save.class)) { //если поле класса помечено аннотацией Save, то
-                    save(field); //для этого поля вызываем метод сохранить
-                    load(field); //для этого поля вызываем метод загрузить
+            Field[] fields = clazz.getDeclaredFields(); //отримуємо список полів класу
+            for (Field field : fields) { //проходимо по кожному полю класу
+                if (field.isAnnotationPresent(Save.class)) { //якщо поле класу позначене анотацією Save, то
+                    save(field); //для цього поля викликаємо метод збереження
+                    load(field); //для цього поля викликаємо метод завантаження
                 }
             }
         } catch (Exception e) {
@@ -41,21 +42,21 @@ public class ClassSearchSave {
         }
     }
 
-    public static void save(Field field) throws Exception { //метод для сохранения
-        //буферизируем звпись в файл
-        try (FileWriter fw = new FileWriter(file, true); //будем писать с добавлением в файл новых данных
+    public static void save(Field field) throws Exception { //метод для збереження
+        //буферизуємо запис у файл
+        try (FileWriter fw = new FileWriter(file, true); //будемо писати з додаванням до файлу нових даних
              BufferedWriter bw = new BufferedWriter(fw);
              PrintWriter writer = new PrintWriter(bw + " ")) {
 
             writer.write(String.valueOf(field));
-            writer.flush(); //пишем если есть еще незаписанные данные
+            writer.flush(); //пишемо, якщо є ще незаписані дані
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void load(Field field) throws Exception { //метод для загрузки
+    public static void load(Field field) throws Exception { //метод для завантаження
         try (BufferedReader reader = new BufferedReader(new FileReader(file));) {
             String str = String.valueOf(field.isAnnotationPresent(Save.class));
             str = reader.readLine();
